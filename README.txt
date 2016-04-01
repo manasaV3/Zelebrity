e324094e3b5e451f4e1982751be9946012d9b259
Manasa <manasa.venkatakrishnan@gmail.com>
2016-03-31 17:33:46 -0700

    Rest of the code

---
+++ README.txt
@@ -0,0 +1,13 @@
+Published Location: http://testingaw.netne.net/manasa/ZessTheZelebrity/
+
+
+Eryn - 
+A Hangman game developed on cpp.
+
+Kyle -
+Zic Zac Zoe
+A java game of Zic Zac Zoe moderated by the robot by Zony Zhei.
+
+Daniel
+Tic Tac Toe
+A java game of Tic Tac Toe.
---
+++ js/l1.js
@@ -0,0 +1,26 @@
+game_question_data = [
+	[
+		{
+			question_img : 'img/britney.jpg',
+			answer : 'britney',
+			answer_img : 'img/britney1.jpg'	
+		},
+		{
+			question_img : 'img/peter.jpg',
+			answer : 'peter',
+			answer_img : 'img/peter1.jpg'	
+		}
+	],
+	[
+		{
+			question_img : 'img/zack.jpg',
+			answer : 'zack',
+			answer_img : 'img/zack1.jpg'	
+		},
+		{
+			question_img : 'img/ian.jpg',
+			answer : 'ian',
+			answer_img : 'img/ian1.jpg'	
+		}
+	]	
+]
